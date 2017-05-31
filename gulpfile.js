cd2cc22264d2808d12f9807b7066d2400ffb6fa4
m-yoshiro <massugu.massugu@gmail.com>
2017-06-01 06:28:53 +0900

    Add styles task with postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,57 @@
+/* ==========================
+
+  Styleguide Build Tasks
+
+  - Config
+  - Stylesheets
+  - Pattern lab
+
+  ========================= */
+
 const gulp = require('gulp');
 const shell = require('gulp-shell');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const scss = require('postcss-scss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+// Config
+//
 
 const PATHS = {
   src: `${__dirname}/source/`,
-  dist: `${__dirname}/public/`
+  dist: `${__dirname}/public/`,
 };
+
+// Stylesheets
+//
+
+gulp.task('styles', () => {
+
+  const AUTOPREFIXER_CONFIG = {
+    browsers: ['last 1 version'],
+  };
+
+  const POSTCSS_CONFIG = {
+    plugins: [
+      AUTOPREFIXER_CONFIG,
+      cssnano(),
+    ],
+    options: {
+      parser: scss,
+    }
+  };
+
+  return gulp.src(`${PATHS.src}/source/css/**/*.scss`)
+    .pipe(sourcemaps.init())
+    .pipe(postcss(POSTCSS_CONFIG))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(`${PATHS.dist}/css/style.css`));
+});
+
+// Pattern Lab
+//
 
 gulp.task('server:patternlab', shell.task([
   'php core/console --server --watch'
